e205fdffb1133a8135d4894f73e7de7b1cf2d5da
Josh Chisholm <joshuachisholm@gmail.com>
2014-12-08 22:01:32 +0000

    restaurant postcode admin

--- app/admin/restaurant.rb
+++ app/admin/restaurant.rb
@@ -1,20 +1,23 @@
 ActiveAdmin.register Restaurant do
-  permit_params :name, :telephone_number
+  permit_params :name, :postcode, :telephone_number
 
   index do
     selectable_column
     id_column
     column :name
+    column :postcode
     column :telephone_number
     actions
   end
 
   filter :name
+  filter :postcode
   filter :telephone_number
 
   form do |f|
     f.inputs "Details" do
       f.input :name
+      f.input :postcode
       f.input :telephone_number
     end
     f.actions
